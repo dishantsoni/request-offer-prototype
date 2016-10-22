c15315487a94c4c0559598e6d40db64cd94f254e
Dishant Soni <sonidishantv@gmail.com>
2016-10-21 19:02:11 -0500

    initial setup with webpack and redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+    return [
+        {title: 'JavaScript: The Good Parts'},
+        {title: 'JavaScript : The Definitive Guide'},
+        {title: 'Thinking in Java'},
+        {title: 'Who moved my cheese'},
+        {title: 'Redux is amazing'}
+    ]
+}
